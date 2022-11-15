57d4ae702946a315ea75bb0b85c341aae45ae508
wiut00012183 <95349072+wiut00012183@users.noreply.github.com>
2022-11-15 15:58:05 +0300

    Special Keys
    This is for the key to move snake 4 directions: up, down, left, right with their names on keyboard.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import turtle
 import time
 import random
 
+# this lines are for the speed and the scores of the snakes movements and eats
 delay = 0.11
 score = 0
 high_score = 0
@@ -15,7 +16,7 @@ win.bgcolor("white")
 win.setup(width=700, height=600)
 win.tracer(0)
 
-# I will write this in order to create the head of the sneake
+# I will write this in order to create the head of the sneak
 head = turtle.Turtle()
 head.shape("circle")
 head.color("green")
@@ -32,6 +33,7 @@ food.color(colors)
 food.penup()
 food.goto(0, 100)
 
+# I will write the lines special for the text that mentions above the project to show the scores
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -41,3 +43,48 @@ pen.hideturtle()
 pen.goto(-100, 250)
 pen.write("Score : 0 High Score : 0", align="center",
           font=("arial", 22, "bold"))
+
+# it is for assigning the 4 types of key directions
+# it is for the snake going up
+def group():
+    if head.direction != "down":
+        head.direction = "up"
+
+# it is for the snake going down
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+
+# it is for the snake going left
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+# it is for the snake going right
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+#it is the calling keys to move snake
+win.listen()
+win.onkeypress(group, "w")
+win.onkeypress(godown, "s")
+win.onkeypress(goleft, "a")
+win.onkeypress(goright, "d")
